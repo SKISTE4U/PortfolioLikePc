2913be25cd544eb12ce0527fe16b438558c9fa89
SKISTE <skiste4u@gmail.com>
2025-07-19 10:41:48 +0400

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+function debug(text) {
+    console.log(text)
+}
+
 function randint(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function show_loading_page(time_for_show = 3, opacity_dur = .5) {
-    let loading_div = document.querySelector('#loading')
     loading_div.style.display = 'flex'
     loading_div.style.animation = `opacity ${opacity_dur}s forwards`
     setTimeout(function(){
@@ -14,6 +17,21 @@ function show_loading_page(time_for_show = 3, opacity_dur = .5) {
         },opacity_dur*1000)
     },time_for_show*1000)
 }
-function go_to_workspace() {
-    show_loading_page(2)
+function go_to_screen(screen) {
+    let loading_dur = .5
+    show_loading_page(2,loading_dur)
+    setTimeout(function(){
+        let all_screens = document.querySelectorAll('.one_screen')
+        for (let x = 0; x < all_screens.length; x++) {
+            const element = all_screens[x];
+            element.style.display = 'none'
+        }
+        let screens_name = ['loading','workspace','login_page_div']
+        let screens = [loading_div,workspace_div,login_page_div]
+
+        debug(screens_name.indexOf(screen))
+        debug(screens[0])
+
+        screens[screens_name.indexOf(screen)].style.display = 'flex'
+    },loading_dur*1000)
 }
---
+++ js/on_load.js
@@ -0,0 +1,3 @@
+const loading_div = document.querySelector('#loading')
+const workspace_div = document.querySelector('#workspace')
+const login_page_div = document.querySelector('#login_page')
